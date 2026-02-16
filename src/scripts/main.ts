5b57e7708732923383e444caa531e13993e63a39
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-02-16 20:19:14 +1000

    feat: add theme switching logic

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,11 @@
 import { initAnimations } from "@scripts/features/animations";
 import { initLenis } from "@scripts/features/scroll";
 import { initFontSelector } from "./features/fontSelector";
+import { initThemeSwitch } from "./features/themeSwitch";
 
 document.addEventListener("DOMContentLoaded", () => {
   initAnimations();
   initLenis();
   initFontSelector();
+  initThemeSwitch();
 });
